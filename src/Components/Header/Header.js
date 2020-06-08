3a7bd6b9aca64eb710465c080b4cd0ded634e792
Purvesh Shende <56558717+purveshshende2@users.noreply.github.com>
2020-06-08 22:31:02 +0530

    Update header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = ({props,onRouteChange}) => {
   return (
     <header className="App-header">
       <div className = 'mylogo'>
-        <a href = 'App' style={{color:'black'}} className = 'one f3 fl pa3 pointer no-underline mt2'><span>▷ </span><b>FindMy!</b>Movies</a>
+        <a href = 'App.js' style={{color:'black'}} className = 'one f3 fl pa3 pointer no-underline mt2'><span>▷ </span><b>FindMy!</b>Movies</a>
       </div>
       <div  style = {{display : 'flex',justifyContent : 'flex-end'}}>
       <a href = 'About.js' style={{color:'black'}} className = 'f3 link dim white  pa3  no-underline mt2'></a>
